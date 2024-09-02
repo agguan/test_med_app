738dc6ef8b3a4db9b07deb4b5f437e45c5c61f15
agguan <andrewg1153@gmail.com>
2024-09-02 02:57:15 -0400

    progress update

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:3OyVvul6jFSXiRJndcGF2Ad4@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:0Uu013DroIyyocGwPqDA0bzp@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
